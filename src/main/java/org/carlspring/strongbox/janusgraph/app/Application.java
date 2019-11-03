ba43c565a04e547501e2879f041d9cb5db980445
Przemyslaw Fusik <przemyslaw.fusik@gmail.com>
2019-11-03 17:14:25 +0100

    janusgraph visualization

--- src/main/java/org/carlspring/strongbox/janusgraph/app/Application.java
+++ src/main/java/org/carlspring/strongbox/janusgraph/app/Application.java
@@ -2,7 +2,9 @@ package org.carlspring.strongbox.janusgraph.app;
 
 import org.carlspring.strongbox.janusgraph.cassandra.CassandraEmbeddedConfig;
 import org.carlspring.strongbox.janusgraph.graph.JanusGraphConfig;
+import org.carlspring.strongbox.janusgraph.graph.gremlin.server.GremlinServerConfig;
 import org.carlspring.strongbox.janusgraph.reposiotries.RepositoriesConfig;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -10,12 +12,14 @@ import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @EnableConfigurationProperties
-@Import({ CassandraEmbeddedConfig.class, JanusGraphConfig.class, RepositoriesConfig.class })
+@Import({ CassandraEmbeddedConfig.class,
+          JanusGraphConfig.class,
+          GremlinServerConfig.class,
+          RepositoriesConfig.class })
 public class Application
 {
 
     public static void main(String[] args)
-        throws Exception
     {
         SpringApplication.run(Application.class, args);
     }
