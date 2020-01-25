cd4f235f031523766dffc8dddfd46e9631ae6882
Ankit Tomar <tomarankit.nitb@gmail.com>
2020-01-25 17:01:35 +0530

    KeyspaceNotDefinedException Fix

--- src/main/java/org/carlspring/strongbox/janusgraph/cassandra/CassandraEmbeddedConfig.java
+++ src/main/java/org/carlspring/strongbox/janusgraph/cassandra/CassandraEmbeddedConfig.java
@@ -1,13 +1,23 @@
 package org.carlspring.strongbox.janusgraph.cassandra;
 
-import java.io.IOException;
+import javax.annotation.PreDestroy;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Comparator;
+import java.util.stream.Stream;
+
+import org.apache.cassandra.config.DatabaseDescriptor;
 import org.apache.cassandra.service.CassandraDaemon;
 import org.apache.cassandra.service.StorageService;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
 
 @Configuration
 @ComponentScan
@@ -34,6 +44,39 @@ public class CassandraEmbeddedConfig
         StorageService.instance.removeShutdownHook();
 
         return cassandraDaemon;
+    }
+
+    @PreDestroy
+    public void destroy()
+        throws IOException
+    {
+        deleteDirectory(DatabaseDescriptor.getRawConfig().commitlog_directory);
+        deleteDirectory(DatabaseDescriptor.getRawConfig().hints_directory);
+        deleteDirectory(DatabaseDescriptor.getRawConfig().saved_caches_directory);
+
+        String[] dataDirectories = DatabaseDescriptor.getRawConfig().data_file_directories;
+        for (String directory : dataDirectories)
+        {
+            deleteDirectory(directory);
+        }
+    }
+
+    private void deleteDirectory(String dataDirectory)
+        throws IOException
+    {
+        if (StringUtils.isEmpty(dataDirectory))
+        {
+            return;
+        }
+
+        Path rootPath = Paths.get(dataDirectory);
+        try (Stream<Path> walk = Files.walk(rootPath))
+        {
+            walk.sorted(Comparator.reverseOrder())
+                .map(Path::toFile)
+                // .peek(System.out::println)
+                .forEach(File::delete);
+        }
     }
 
 }
